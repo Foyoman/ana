258d503da4ee4a81806b8bc61b65843e46cb63fa
Edward Do <edwardtdo@gmail.com>
2022-07-23 18:09:04 +1000

    styled the pages, added a new+create for anecdotes,  fixed formatting

--- app/controllers/anecdotes_controller.rb
+++ app/controllers/anecdotes_controller.rb
@@ -1,13 +1,24 @@
 class AnecdotesController < ApplicationController
   def index
+    @anecdotes = Anecdote.all
   end
 
   def show
   end
 
   def new
+    @anecdote = Anecdote.new
+  end
+
+  def create
+    anecdote = Anecdote.create anecdote_params
   end
 
   def edit
+  end
+
+  private
+  def mixtape_params
+    params.require(:anecdote).permit(:title, :content, :tag)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -16,9 +16,16 @@ t2 = Tag.create :name => 'relationship'
 t3 = Tag.create :name => 'academia'
 t4 = Tag.create :name => 'loss of a loved one'
 t5 = Tag.create :name => 'body'
-t6 = Tag.create :name => 'pet'
+t6 = Tag.create :name => 'pets'
 t7 = Tag.create :name => 'anxiety'
 t8 = Tag.create :name => 'depression'
+t9 = Tag.create :name => 'phobias'
+t10 = Tag.create :name => 'childhood'
+t11 = Tag.create :name => 'funny'
+t12 = Tag.create :name => 'life lessons'
+t13 = Tag.create :name => 'health'
+t14 = Tag.create :name => 'work'
+t15 = Tag.create :name => 'friends'
 puts "#{ Tag.count } tags"
 
 # End tags #
@@ -27,17 +34,17 @@ puts "#{ Tag.count } tags"
 
 Anecdote.destroy_all
 
-d1 = Anecdote.create :title => 'test d1', :content => 'test d1 content family relationship'
-d1.tags << t1 << t2
+d1 = Anecdote.create :title => 'Your life, right now', :content => %(Two years ago I was having an awful time during a major bout of depression in combination with a problem I was having physically that my doctor was telling me to ”Wait and see if it goes away on its own. If it’s still happening a month from now we’ll run some tests” on repeat for over a year. I wish I could go back and tell myself that things are bad, but that life is still there to be enjoyed if you’re present. To be in my body and not my head so that I could notice the nice moments that pop up even in the worst parts of your life. Waiting till things are good sometimes means waiting a really long time, but your life is happening right now. Be there for it.)
+d1.tags << t5 << t8 << t13
 
-d2 = Anecdote.create :title => 'test d2', :content => 'test d2 content anxiety'
-d2.tags << t7
+d2 = Anecdote.create :title => 'Fear of the dark', :content => 'I have fear of the dark. So when I was little my parents went to some sort of ceremony. Load shedding happened means all the lights went off I was the only one at home. Could not stop crying even my parents held me.'
+d2.tags << t9 << t10
 
-d3 = Anecdote.create :title => 'test d3', :content => 'test d3 content loss of a loved one family'
-d3.tags << t1 << t4
+d3 = Anecdote.create :title => 'Sometimes its good not to force things', :content => 'One time I tried to fart, but what came out was not a fart.'
+d3.tags << t11 << t12 
 
-d4 = Anecdote.create :title => 'test d4', :content => 'test d4 content body'
-d4.tags << t5
+d4 = Anecdote.create :title => "Work to live, don't live to work", :content => 'Your life outside of work is equally as important. After going through a little work rut (feeling resentful and negative about long hours etc) I realised that I need to prioritise seeing my family, friends and making the time to do the things I love like cooking and pottery.'
+d4.tags << t1 << t14 << t15
 
 d5 = Anecdote.create :title => 'test d5', :content => 'test d5 content family'
 d5.tags << t1
